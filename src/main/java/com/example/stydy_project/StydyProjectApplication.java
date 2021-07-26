3ff01893c7e7b1417edad23ae25de29467438db5
haejii <yanghagi@naver.com>
2021-07-26 22:36:33 +0900

    message

---
+++ src/main/java/com/example/stydy_project/StydyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.stydy_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StydyProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StydyProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/stydy_project/StydyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stydy_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StydyProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
